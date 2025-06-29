c8946764854102c840c511cf46445e419c96ef93
Olga Vashurina <ovashurina@gmail.com>
2025-06-29 21:48:26 +0200

    new

--- run-tests.bat
+++ run-tests.bat
@@ -1,2 +1,2 @@
 @echo off
-newman run My_Capital_QA.postman_collection.json -r cli,html
+newman run My_Capital_QA.postman_collection.json --reporters cli,html --reporter-html-export newman/newman-report.html
