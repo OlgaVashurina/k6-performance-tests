c3ec77df79853308e9f63b88b13e39067b216631
Olga Vashurina <ovashurina@gmail.com>
2025-06-29 17:58:38 +0200

    Add k6 test script and results

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  vus: 10, // количество виртуальных пользователей
-  duration: '30s', // длительность теста
+  vus: 10, //
+ duration: '30s',
 };
 
 export default function () {
-  http.get('https://test.k6.io'); // можешь заменить на свой URL
+  http.get('https://test.k6.io');
   sleep(1);
 }
