da6cb53417b0014050febac656780c8a81495376
Olga Vashurina <ovashurina@gmail.com>
2025-07-01 22:24:17 +0200

    new

--- generate-k6-report.js
+++ generate-k6-report.js
@@ -1,10 +1,6 @@
 const reporter = require('k6-html-reporter');
-const fs = require('fs');
-
-const input = 'k6_tests/k6-report.json';
-const output = 'k6_tests/k6-report.html';
-
-const json = JSON.parse(fs.readFileSync(input, 'utf8'));
-reporter.generateHTMLReport(json, output);
-
-console.log('✅ HTML report generated at', output);
+const options = {
+    jsonFile: 'k6_tests/k6-report.json',
+    output: 'k6_tests/k6-report.html',
+};
+reporter.generateSummaryReport(options);
--- scripts/run-k6-tests.bat
+++ scripts/run-k6-tests.bat
@@ -1,6 +1,6 @@
 @echo off
 cd C:\Users\ovash\Desktop\load\k6-performance-tests
 k6 run --out json=k6_tests\k6-report.json k6_tests\test-post-users.js
-npx k6-html-reporter --json k6_tests\k6-report.json --output k6_tests\k6-report.html
+node generate-k6-report.js
 xcopy k6_tests\k6-report.html ..\qa-allure-reports /Y
 pause
