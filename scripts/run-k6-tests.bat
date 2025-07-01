ee7e9eddfe7cd85891105758e8f1108a7f5e0cba
Olga Vashurina <ovashurina@gmail.com>
2025-07-01 19:46:15 +0200

    new

--- scripts/run-k6-tests.bat
+++ scripts/run-k6-tests.bat
@@ -1,12 +1,5 @@
 @echo off
-echo Running k6 performance tests...
-k6 run k6_tests\test-post-users.js
-
-echo Generating HTML report from k6-report.json...
-node C:\Users\ovash\npm-global\node_modules\k6-html-reporter\bin\cli.js --json k6_tests\k6-report.json --output k6_tests\k6-report.html
-
-echo Copying HTML report to qa-allure-reports repo...
+k6 run --out json=k6_tests\k6-report.json k6_tests\test-post-users.js
+node -e "const r = require('k6-html-reporter'); r('k6_tests\\k6-report.json', 'k6_tests\\k6-report.html');"
 xcopy k6_tests\k6-report.html ..\qa-allure-reports /Y
-
-echo All done.
 pause
