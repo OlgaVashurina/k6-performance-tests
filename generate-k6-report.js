bad3b9c56e1329f70938fb95ee17c4f68fa130b0
Olga Vashurina <ovashurina@gmail.com>
2025-07-01 21:49:34 +0200

    new

---
+++ generate-k6-report.js
@@ -0,0 +1,7 @@
+const reporter = require('k6-html-reporter');
+const options = {
+  jsonFile: 'k6_tests/k6-report.json',
+  output: 'k6_tests/k6-report.html'
+};
+
+reporter.generateSummaryReport(options);
--- scripts/run-k6-tests.bat
+++ scripts/run-k6-tests.bat
@@ -1,5 +1,5 @@
 @echo off
 k6 run --out json=k6_tests\k6-report.json k6_tests\test-post-users.js
-node -e "require('./node_modules/k6-html-reporter')('k6_tests\\k6-report.json', 'k6_tests\\k6-report.html')"
+node generate-k6-report.js
 xcopy k6_tests\k6-report.html ..\qa-allure-reports /Y
 pause
