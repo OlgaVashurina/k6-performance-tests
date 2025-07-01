db9b9fd9aff2820f8dd307559608e34c42c653af
Olga Vashurina <ovashurina@gmail.com>
2025-07-01 18:58:01 +0200

    new

---
+++ k6_tests/test-post-users.js
@@ -0,0 +1,26 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  vus: 1,
+  iterations: 5,
+};
+
+export default function () {
+  const payload = JSON.stringify({
+    name: 'Demo User',
+    username: 'demouser',
+    email: 'demo@example.com'
+  });
+
+  const headers = { 'Content-Type': 'application/json' };
+
+  let res = http.post('https://jsonplaceholder.typicode.com/users', payload, { headers });
+
+  check(res, {
+    'status is 201': (r) => r.status === 201
+  });
+
+  console.log('Server Status:', res.status);
+  sleep(1);
+}
