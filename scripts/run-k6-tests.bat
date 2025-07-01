1c6d051ae1ae55d96a205d40d8f1168e1ebd5ff0
Olga Vashurina <ovashurina@gmail.com>
2025-07-01 19:52:12 +0200

    new

--- scripts/run-k6-tests.bat
+++ scripts/run-k6-tests.bat
@@ -1,5 +1,5 @@
 @echo off
 k6 run --out json=k6_tests\k6-report.json k6_tests\test-post-users.js
-node -e "const r = require('k6-html-reporter'); r('k6_tests\\k6-report.json', 'k6_tests\\k6-report.html');"
+node -e "require('./node_modules/k6-html-reporter')('k6_tests\\k6-report.json', 'k6_tests\\k6-report.html')"
 xcopy k6_tests\k6-report.html ..\qa-allure-reports /Y
 pause
