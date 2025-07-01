f78453050a003c9b17ec10c163c7393478087c2e
Olga Vashurina <ovashurina@gmail.com>
2025-07-01 22:04:57 +0200

    new

--- scripts/run-k6-tests.bat
+++ scripts/run-k6-tests.bat
@@ -1,6 +1,6 @@
 @echo off
 cd C:\Users\ovash\Desktop\load\k6-performance-tests
 k6 run --out json=k6_tests\k6-report.json k6_tests\test-post-users.js
-node generate-k6-report.js
+C:\Users\ovash\npm-global\node_modules\.bin\k6-html-reporter --json k6_tests\k6-report.json --output k6_tests\k6-report.html
 xcopy k6_tests\k6-report.html ..\qa-allure-reports /Y
 pause
