4348c957df6827a9316a71844945e03d7e8a9957
Olga Vashurina <ovashurina@gmail.com>
2025-07-04 15:20:44 +0200

    new

--- scripts/run-k6-tests.bat
+++ scripts/run-k6-tests.bat
@@ -2,5 +2,5 @@
 cd C:\Users\ovash\Desktop\load\k6-performance-tests
 k6 run --out json=k6_tests\k6-report.json k6_tests\test-post-users.js
 node generate-k6-report.js
-xcopy k6_tests\k6-report.html ..\qa-allure-reports /Y
+
 pause
--- scripts/run-tests.bat
+++ scripts/run-tests.bat
@@ -6,8 +6,5 @@ newman run postman\My_Capital_QA.postman_collection.json -r cli,allure --reporte
 echo Generating Allure report...
 allure generate allure-results -o allure-report --clean
 
-echo Copying report to qa-allure-reports repo...
-xcopy allure-report\* ..\qa-allure-reports /E /H /C /I /Y
-
 echo All done!
 pause
